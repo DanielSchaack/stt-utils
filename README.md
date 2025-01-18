297ee36455468ccfdff9bf932c47d282fa8e4e46
Daniel Schaack <d.schaack@web.de>
2025-01-18 18:50:42 +0100

    added initial producer/consumer setup

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# speech-to-clipboard
+# Speech-to-Clipboard
 Speech to Clipboard
+
+
+# To Do
+
+## Must do
+- start and send data to whisper
+- add a worker thread for stream processing
+- transfer chunks to worker thread
+- concatenate chunked data
+- use sliding window to transfer word by word
+    - print continuous results to console
+
+- add result to clipboard
+
+## High
+- find a way to toggle/ start-end recording
+- add logic to identify recognized words by x repeats in sliding window
+
+## Mid
+- differentiate between recognized and potential words
+
+## Optional - Frontend
+When logic stands, add simple frontend to display recognized/potential words
+## Optional - Background Service
+When done, find a way to start as a continuous background service
+## Optional - Optimizations
+Optimise uptime of processing/whisper etc.
--- voice.py
+++ voice.py
@@ -1,12 +1,11 @@
-import wave
 import sys
 import time
-from typing import Any, Tuple
-
-# import queue
+import threading
+import queue
 
 import pyaudio
 
+q = queue.Queue()
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1 if sys.platform == 'darwin' else 2
@@ -14,75 +13,16 @@ RATE = 44100
 RECORD_SECONDS = 15
 
 
-
-def record(filename: str):
-    with wave.open(filename, 'wb') as wf:
-        p = pyaudio.PyAudio()
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-
-        stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
-
-        print('Recording...')
-        for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
-            wf.writeframes(stream.read(CHUNK))
-        print('Done')
-
-        stream.close()
-        p.terminate()
-
-
 def callback_send_to_queue(in_data, frame_count, time_info, status):
     # print(f"in_data: {in_data}")
-    print(f"frame_count: {frame_count}")
-    print(f"time_info: {time_info}")
-    print(f"status: {status}")
+    # print(f"frame_count: {frame_count}")
+    # print(f"time_info: {time_info}")
+    # print(f"status: {status}")
+    print("inside of callback")
+    item = (in_data, frame_count)
+    q.put(item=item)
     return (in_data, pyaudio.paContinue)
 
-
-def callback_replay_input(in_data, frame_count, time_info, status) -> Tuple[Any, int]:
-    """
-    Args:
-        in_data: Array of byte data. None if stream has input=False
-        frame_count: Amount of frames inside buffered input data (in_data)
-        time_info:
-                - 1 replay timestamp if replay
-                - 2 idk
-                - 3 idk
-        status:
-                - 0 no issues(more data incoming)
-                - 1 input underflow
-                - 2 input overflow
-                - 3 output underflow
-                - 4 output overflow
-                - 16 priming
-
-    Returns:
-        tuple (output_data, statuscode)
-    """
-    # print(f"in_data: {in_data}")
-    print(f"frame_count: {frame_count}")
-    print(f"time_info: {time_info}")
-    print(f"status: {status}")
-    return (in_data, pyaudio.paContinue)
-
-
-# TODO
-# - find a way to toggle/ start-end recording
-# - add a worker thread for stream processing
-# - transfer chunks to worker thread
-# - concatenate chunked data
-# - start and send data to whisper
-# - print continuous results to console
-# - use sliding window to transfer word by word
-# - add logic to identify recognized words by x repeats in sliding window
-# - differentiate between recognized and potential words
-# - add result to clipboard
-
-# Optional: When logic stands, add simple frontend to display recognized/potential words
-# Optional: When done, find a way to start as a continuous background service
-# Optional: Optimise uptime of processing/whisper etc
 
 def record_replay():
     p = pyaudio.PyAudio()
@@ -106,53 +46,28 @@ def record_replay():
     p.terminate()
 
 
-def replay(filename: str):
-    with wave.open(filename, 'rb') as wf:
-        p = pyaudio.PyAudio()
+def producer():
+    print("Starting worker thread")
+    record_replay()
 
-        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                        channels=wf.getnchannels(),
-                        rate=wf.getframerate(),
-                        # input=True,
-                        output=True,
-                        frames_per_buffer=512)
 
-        while len(data := wf.readframes(CHUNK)):
-            stream.write(data)
-
-        stream.close()
-
-        p.terminate()
+def consumer():
+    print("Starting worker thread")
+    while True:
+        print(f"Queue size: {q.qsize()}")
+        if not q.empty():
+            (in_data, frame_count) = q.get()
+            print(f"Frame count: {frame_count}")
+            q.task_done()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
-    record_replay()
+    producer_thread = threading.Thread(target=producer, args=())
+    producer_thread.start()
+    consumer_thread = threading.Thread(target=consumer, args=())
+    consumer_thread.start()
+
     # replay('output.wav')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
