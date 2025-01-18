35d38c82cdd345fbbb872065165e3df0355025a7
Daniel Schaack <d.schaack@web.de>
2025-01-18 18:40:23 +0100

    Added testing with whisper

---
+++ recording.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class recording():
+    def __init__(self):
+        self.data = np.empty(dtype=np.int16)
+        self.window_bottom = 0
+        self.window_top = 0
+        self.validated_words = []
+        self.words_in_question = []
+
+    def addChunk(self, chunk):
+        self.data = np.append(self.data, chunk)
+
+    def transcribe(self):
+        pass
+
+    def validate_words(self):
+        pass
+
---
+++ testing.py
@@ -0,0 +1,132 @@
+import wave
+import pyaudio
+import sys
+import time
+import gc
+import torch
+from typing import Tuple, Any
+
+import numpy as np
+import whisper
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1 if sys.platform == 'darwin' else 2
+RATE = 16000
+CHUNK = RATE
+RECORD_SECONDS = 15
+
+SAMPLE_RATE = 16000
+
+
+def record(filename: str):
+    with wave.open(filename, 'wb') as wf:
+        p = pyaudio.PyAudio()
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+
+        stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
+
+        print('Recording...')
+        for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
+
+            wf.writeframes(stream.read(CHUNK))
+        print('Done')
+
+        stream.close()
+        p.terminate()
+
+
+def replay(filename: str):
+    with wave.open(filename, 'rb') as wf:
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=pyaudio.paInt16,
+                        channels=CHANNELS,
+                        rate=SAMPLE_RATE,
+                        # input=True,
+                        output=True,
+                        frames_per_buffer=CHUNK)
+
+        while len(data := wf.readframes(CHUNK)):
+            stream.write(data)
+
+        stream.close()
+
+        p.terminate()
+
+
+def transcribe():
+    model = whisper.load_model("turbo")
+    # result = model.transcribe(audio=filename, language="de", verbose=True)
+    # print(result["text"])
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=CHANNELS,
+                    rate=SAMPLE_RATE,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=CHUNK)
+
+    print("Start reading")
+    total_data = b""
+
+    while True:
+        data = stream.read(CHUNK)
+        print("CHUNK read")
+        total_data += data
+        stream.write(data)
+        print("CHUNK replayed")
+        np_data = np.frombuffer(total_data, np.int16).flatten().astype(np.float32) / 32768.0
+        result = model.transcribe(audio=np_data,
+                                  language="en",
+                                  beam_size=5,
+                                  fp16=False,
+                                  verbose=True)
+        # print(result["text"])
+
+    stream.close()
+
+    p.terminate()
+
+
+def callback_replay_input(in_data, frame_count, time_info, status) -> Tuple[Any, int]:
+    """
+    Args:
+        in_data: Array of byte data. None if stream has input=False
+        frame_count: Amount of frames inside buffered input data (in_data)
+        time_info:
+                - 1 replay timestamp if replay
+                - 2 idk
+                - 3 idk
+        status:
+                - 0 no issues(more data incoming)
+                - 1 input underflow
+                - 2 input overflow
+                - 3 output underflow
+                - 4 output overflow
+                - 16 priming
+
+    Returns:
+        tuple (output_data, statuscode)
+    """
+    # print(f"in_data: {in_data}")
+    print(f"frame_count: {frame_count}")
+    print(f"time_info: {time_info}")
+    print(f"status: {status}")
+    return (in_data, pyaudio.paContinue)
+
+
+if __name__ == "__main__":
+    try:
+        # record("output.wav")
+        # replay("output.wav")
+        transcribe()
+    except Exception as e:
+        print(e)
+    finally:
+        # del model
+        torch.cuda.empty_cache()
+        gc.collect()
